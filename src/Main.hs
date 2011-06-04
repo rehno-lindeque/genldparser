887c0104f57bdf1b273421f838ececf639c0c177
Rehno Lindeque <errantkid@gmail.com>
2011-06-04 16:09:26 +0200

    Initial setup for the parser generator

---
+++ src/Main.hs
@@ -0,0 +1,20 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Main
+-- Copyright   :  Rehno Lindeque, 2011
+-- License     :  BSD3
+--
+-- Maintainer  :  Rehno Lindeque
+-- Stability   :  Unstable
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Main (
+    main
+) where
+
+main = putStrLn ""
+
